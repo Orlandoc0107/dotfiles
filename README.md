bd05e946a30faf6d8f80dc0557d4769907e7d39e
orlandoc0107 <orlandocardenas0107@gmail.com>
2023-02-04 19:37:08 -0300

    Actualizacion

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# dotfiles
-mi dotfiles ArchLinux 
+# dotfiles-archlinux
+
+- Muchisimas gracias a la Comunidad linuxera
+
+Nota : Los doffiles estan personalizados, claramente se puede modificar o editar una vez instales . 
+
+La scrip se debe ejecutar despues de instalar los siguientes complementos necesatios para ejecutar Archlinux
+talez como : base base-devel linux linux-firware efibootmgr 
+
+Recomendaciones : Descargar laimagen ISO de ArchLinux en lapagina oficial https://archlinux.org/ 
+
+uan vez que logres bootear tu sistema puedes hacer una isntalacion automatica ejecutando archinstall
+
+Video 1 
+
+https://youtu.be/JjMdSukK3k4
+
+
+
+importante no tener otro gestor de inicio de seccion preinstalado asi te ahorraras un dolor de cabezas
+
+compartible con ArchLinux
+
+para usar instalacion automatica descargue instalar.sh con git 
+
+sudo pacman -S git
+
+* instalar.sh 
+
+git clone https://github.com/Orlandoc0107/dotfiles-archlinux.git 
+
+cd dotfiles-archlinux
+sudo chmod+x 
+./instalar.sh
+
+Listado de las aplicaciones que se instaaran automaticamente ( si desean remover alguna aplicacion lo pueden hacer con {Pacman -R Nombre-Del-Paquete})
+
+Pacman -S git lightdm lightdm-gtk-greeter dhcpcd networkmanager kitty xterm lsd bat xorg xorg-apps xorg-xinit xorg-twm xorg-xclock pulseaudio pavucontrol arc-gtk-theme arc-icon-theme code nano vim kate firefox lynx w3m neofetch lsb-release bspwm sxhkd dmenu polybar flex imagemagick lxappearance nitrogen picom rofi thunar thunar-volman udiskie spotify-launcher android-file-transfer android-tools android-udev msmtp libmtp libcddb gvfs gvfs-afc gvfs-smb gvfs-gphoto2 gvfs-mtp gvfs-goa gvfs-nfs gvfs-google dosfstools jfsutils f2fs-tools btrfs-progs exfat-utils ntfs-3g reiserfsprogs udftools xfsprogs nilfs-utils polkit gpart mtools ark xarchiver unarchiver binutils gzip lha lrzip lzip lz4 p7zip tar xz bzip2 p7zip lbzip2 arj lzop cpio unrar unzip zstd zip lzip unarj zstd sudo pacman -S ffmpeg aom libde265 x265 x264 libmpeg2 xvidcore libtheora libvpx schroedinger sdl gstreamer gst-plugins-bad gst-plugins-base gst-plugins-base-libs gst-plugins-good gst-plugins-ugly xine-lib libdvdcss libdvdread dvd+rw-tools lame vlc 
+
+
+
+yay -S backlight_control 
---
+++ instalar.sh
@@ -0,0 +1,269 @@
+#!/bin/bash
+# Autor : Orlandoc0107
+# Instlacion automatica de mi configuracion
+clear
+echo "----"
+echo "----"
+echo "----"
+echo "----"
+echo "----"
+echo "----"
+echo "----"
+echo "----"
+echo "----"
+echo "----"
+echo "----"
+echo "----"
+echo "----"
+echo "----"
+echo "_________________________________________________________________________________________"	
+echo "|#######                                                    ###     #     ###   ####### |" 
+echo "|#     # #####  #        ##   #    # #####   ####   ####   #   #   ##    #   #  #    #  |"  
+echo "|#     # #    # #       #  #  ##   # #    # #    # #    # #     # # #   #     #     #   |"   
+echo "|#     # #    # #      #    # # #  # #    # #    # #      #     #   #   #     #    #    |"    
+echo "|#     # #####  #      ###### #  # # #    # #    # #      #     #   #   #     #   #     |"     
+echo "|#     # #   #  #      #    # #   ## #    # #    # #    #  #   #    #    #   #    #     |"     
+echo "|####### #    # ###### #    # #    # #####   ####   ####    ###   #####   ###     #     |"
+echo "|_______________________________________________________________________________________|"
+echo "----"
+echo "----"
+echo "----"
+echo "----"
+echo "----"
+echo "----"
+echo "----"
+echo "----"
+echo "----"
+echo "----"
+echo "----"
+echo "----"
+echo "----"
+echo "----"
+echo " Presione otra Tecla para Continuar "
+read
+clear
+############ 
+cd $HOME
+echo "Preparando instalacion de yay"
+sudo pacman -S git --noconfirm --needed
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si --noconfirm --needed
+echo "Instalacion de yay completada"
+clear
+#gestor de Inicio de seccion
+cd $HOME
+echo "Instalacion del gestor de inicio de seccion Lightdm"
+sudo pacman -S lightdm lightdm-gtk-greeter --noconfirm --needed 
+echo "Instalacion del gestor completada "
+echo "configurando"
+pwd
+cd $HOME
+cd dotfiles-archlinux
+cd lightdm 
+sudo cp lightdm.conf /etc/lightdm/
+##############################
+clear
+cd $HOME
+pwd 
+echo "ATENCION !"
+echo "Habilitando el Gestor lightdm asegurese de no tener otro gestor habilitado"
+echo "use este comando para desactivar el otro gestor si tiene problemas"
+echo "ejemplo sudo systemctl disable nombre-de-la-aplicacion.service"
+echo "Presione una tecla para continuar"
+read 
+sudo systemctl enable lightdm.service 
+echo "Para prevenir cualquier error porfavor Habilite manualmente el Lighthm.service Manualmente"
+echo "Ingrese el siguiente comando sin los simbolos #, con permisos root "
+echo "# systemctl enable lightdm.service #"
+read  
+echo "Presione otra tecla para continuar "
+echo " Systemctl habilito el gestor Lightdm "
+# intalacion de networkManager
+echo "Asegurando la coneccion wifi" 
+sudo pacman -S dhcpcd networkmanager --noconfirm --needed
+sudo systemctl enable dhcpcd 
+sudo systemctl enable NetworkManager
+sudo systemctl start dhcpcd 
+sudo systemctl start NetworkManager 
+clear 
+echo "Presione una tecla para continuar"
+read 
+echo "Servicios Habilitado"
+echo "se usar nmcli"
+nmcli device
+echo "|--------------------------------------------------------|"
+echo "|  Ingrese el nombre de la red Wifi que desea conectarse |"
+echo "|________________________________________________________|"
+read nombredered
+echo "|--------------------------------------------------------|"
+echo "|           Ingrese el la contrasena de la clave         |"
+echo "|________________________________________________________|"
+read clave
+echo "|--------------------------------------------------------|"
+nmcli dev wifi connect $nombredered password $clave
+echo ""
+echo "Presione una tecla para continuar."
+read
+clear 
+# instalando aplicaciones
+echo "|--------------------------------------------------------|"
+echo "|   Instalando Emuladores de Terminales Kitty y Xterm    |"
+echo "|--------------------------------------------------------|"
+sudo pacman -S kitty xterm lsd bat --noconfirm --needed
+clear 
+echo "|--------------------------------------------------------|"
+echo "|            asegurando la instlacion de xorg            |"
+echo "|--------------------------------------------------------|"
+sudo pacman -S xorg xorg-apps xorg-xinit xorg-twm xorg-xclock --noconfirm --needed
+clear
+echo "|--------------------------------------------------------|"
+echo "|           Instalando PulseAudio y Pavucontrol          |"
+echo "|--------------------------------------------------------|"
+sudo pacman -S pulseaudio pavucontrol --noconfirm --needed
+clear 
+echo "|--------------------------------------------------------|"
+echo "|                Themes Incoos Arch Linux                |"
+echo "|--------------------------------------------------------|"
+sudo pacman -S arc-gtk-theme arc-icon-theme --noconfirm --needed
+clear 
+echo "|--------------------------------------------------------|"
+echo "|     Cuida de ojos , Instalando Backlight_Control       |"
+echo "|--------------------------------------------------------|"
+yay -S backlight_control 
+clear
+echo "|--------------------------------------------------------|"
+echo "|                  Instalando Editores                   |"
+echo "|--------------------------------------------------------|"
+echo "Intalando Editores"
+sudo pacman -S code nano vim kate --noconfirm --needed
+clear
+echo " Instalando Navegadores de internet"
+echo "|--------------------------------------------------------|"
+echo "|               Instalando Firefox lynx w3m              |"
+echo "|--------------------------------------------------------|"
+sudo pacman -S firefox lynx w3m --noconfirm --needed
+clear 
+echo "|--------------------------------------------------------|"
+echo "|                    Instalando Neofetch                 |"
+echo "|--------------------------------------------------------|"
+sudo pacman -S neofetch lsb-release --noconfirm --needed
+clear 
+neofetch 
+echo "Presione alguna tecla para continuar ..."
+read 
+clear
+#Instalacion de BSPWM
+echo "|--------------------------------------------------------|"
+echo "|                       BSPWM                            |"
+echo "|--------------------------------------------------------|"
+sudo pacman -S bspwm sxhkd --noconfirm
+sudo pacman -S sxhkd --noconfirm 
+sudo pacman -S polybar --noconfirm 
+sudo pacman -S dmenu --noconfirm 
+sudo pacman -S flex imagemagick lxappearance nitrogen picom rofi thunar thunar-volman udiskie --noconfirm
+echo 
+echo "|--------------------------------------------------------|"
+echo "|                        Spotify                         |"
+echo "|--------------------------------------------------------|"
+echo "Instalando Spotify"
+sudo pacman -S spotify-launcher --noconfirm
+echo "----------------------------------"
+clear
+echo "|--------------------------------------------------------|"
+echo "|           Instalando Formatos de discos                |"
+echo "|--------------------------------------------------------|"
+echo "Grcias a Codigo Cristo por la recopilacion de las extensiones que debe ser instaladas "
+sudo pacman -S android-file-transfer android-tools android-udev msmtp libmtp libcddb gvfs gvfs-afc gvfs-smb gvfs-gphoto2 gvfs-mtp gvfs-goa gvfs-nfs gvfs-google dosfstools jfsutils f2fs-tools btrfs-progs exfat-utils ntfs-3g reiserfsprogs udftools xfsprogs nilfs-utils polkit gpart mtools --noconfirm --needed
+clear
+echo "|--------------------------------------------------------|"
+echo "|         Instalando Compresion y Descompresion          |"
+echo "|--------------------------------------------------------|"
+echo "Instalando Compresion y Descompresion "
+sudo pacman -S ark xarchiver unarchiver binutils gzip lha lrzip lzip lz4 p7zip tar xz bzip2 p7zip lbzip2 arj lzop cpio unrar unzip zstd zip lzip unarj zstd --noconfirm --needed 
+clear 
+echo "|--------------------------------------------------------|"
+echo "|    Instalando Codecs de Videos + Reproductor VLC       |"
+echo "|--------------------------------------------------------|"
+echo "Instalando Codecs de Videos"
+sudo pacman -S ffmpeg aom libde265 x265 x264 libmpeg2 xvidcore libtheora libvpx schroedinger sdl gstreamer gst-plugins-bad gst-plugins-base gst-plugins-base-libs gst-plugins-good gst-plugins-ugly xine-lib libdvdcss libdvdread dvd+rw-tools lame --noconfirm --needed 
+sudo pacman -S vlc --noconfirm --needed
+clear 
+################################
+#Dotfiles
+echo "|--------------------------------------------------------|"
+echo "|        Instalando y Configurando los Dotfiles          |"
+echo "|--------------------------------------------------------|"
+################################
+pwd
+cd $HOME
+cp $HOME/dotfiles-archlinux/picom.conf $HOME/.config
+pwd
+cd $HOME
+touch .xprofile
+echo "udiskie &" >> .xprofile
+echo "sxhkd &" >> .xprofile
+echo "picom &" >> .xprofile
+echo "nitrogen --restore &" >> .xprofile
+echo "exec baspwm" >> .xprofile
+echo "cbattiocon &" >> .xprofile
+#################################
+cd $HOME
+pwd
+mkdir .config
+cd .config
+pwd
+mkdir bspwm
+mkdir polybar
+mkdir sxhkd
+mkdir nitrogen
+mkdir fondos
+mkdir kitty
+
+cd $HOME/dotfiles-archlinux
+cd bspwm 
+cp bspwmrc $HOME/.config/bspwm
+#
+cd $HOME/dotfiles-archlinux
+cd polybar
+cp config.ini $HOME/.config/polybar
+#
+cd $HOME/dotfiles-archlinux
+cd sxhkd 
+cp sxhldrc $HOME/.config/sxhkf
+#
+cd $HOME/dotfiles-archlinux
+cd nitrogen 
+cp bg-saved.cfg $HOME/.config/nitrogen
+cp nitrogen.cfg $HOME/.config/nitrogen 
+#
+cd $HOME/dotfiles-archlinux
+cd fondos/linux-arch-linux-1920x1080-technology-linux-hd-art-wallpaper-preview.jpg $HOME/.config/fondos
+#
+cd $HOME/dotfiles-archlinux
+cd kitty
+cp kitty.conf $HOME/.config/kitty
+#
+cd $HOME/dotfiles-archlinux
+cd Adwaita
+sudo cp cursors/* /usr/share/icons/Adwaita/cursors/
+#
+cd $HOME/dotfiles-archlinux
+cd fonts
+sudo cp Terminus /usr/share/fonts/
+
+
+echo "Configuracion finalizada"
+echo "_________________________________________________________________________________________"	
+echo "|#######                                                    ###     #     ###   ####### |" 
+echo "|#     # #####  #        ##   #    # #####   ####   ####   #   #   ##    #   #  #    #  |"  
+echo "|#     # #    # #       #  #  ##   # #    # #    # #    # #     # # #   #     #     #   |"   
+echo "|#     # #    # #      #    # # #  # #    # #    # #      #     #   #   #     #    #    |"    
+echo "|#     # #####  #      ###### #  # # #    # #    # #      #     #   #   #     #   #     |"     
+echo "|#     # #   #  #      #    # #   ## #    # #    # #    #  #   #    #    #   #    #     |"     
+echo "|####### #    # ###### #    # #    # #####   ####   ####    ###   #####   ###     #     |"
+echo "|_______________________________________________________________________________________|"
+echo " "
+read
+echo "Preione una tecla para Reiniciar el Sistema "
+reboot 
